ca6e2d45e8380fdc50cdd6d5323a0b1a3c78273a
valery <8alery@gmail.com>
2019-04-12 14:30:08 +0300

    Deploy settings

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
 	runtimeCompiler:false,
 	configureWebpack: {
 		devtool: '#eval'
-	}
+	},
+	publicPath: process.env.NODE_ENV === 'production'
+		? '/investment-portfolio/'
+		: '/'
 };
